df1719d84d26c7a54af1e30a0cfe17cc11492381
Nikhil Kshirsagar <nkjava2050@gmail.com>
2019-10-30 13:25:11 +0530

    Condition to check if 'results' array has 'vulnerabilities'

--- json_xml_convertor.py
+++ json_xml_convertor.py
@@ -17,27 +17,28 @@ for file_element in json_data['data'] :
         count +=1
 	#print('result_element[4] :: ',result_element[4])
         #print('vulnerabilities :: ',result_element[vulnerabilities])
-        for vulnerability_ele in result_element["vulnerabilities"]:
-            #print('Vulnerability :: ',vulnerability_ele)
-            cve_message = vulnerability_ele['identifiers']['CVE'][0]
-            info_text = vulnerability_ele['info']
-            #info_text = (*vulnerability_ele['info'], sep = "\n")
-            severity_classname = vulnerability_ele['severity']
-            #print("file_name : ", file_name)
-            #print("cve_message : ", cve_message)
-            #print("info_text : ", info_text)
-            #print("info_text : ", str(info_text))
-            #print("info_text : ", *info_text, sep = "\n")
-            #print("severity_classname : ",severity_classname,"\n----")
+        if 'vulnerabilities' in result_element :
+            for vulnerability_ele in result_element['vulnerabilities']:
+                #print('Vulnerability :: ',vulnerability_ele)
+                cve_message = vulnerability_ele['identifiers']['CVE'][0]
+                info_text = vulnerability_ele['info']
+                #info_text = (*vulnerability_ele['info'], sep = "\n")
+                severity_classname = vulnerability_ele['severity']
+                #print("file_name : ", file_name)
+                #print("cve_message : ", cve_message)
+                #print("info_text : ", info_text)
+                #print("info_text : ", str(info_text))
+                #print("info_text : ", *info_text, sep = "\n")
+                #print("severity_classname : ",severity_classname,"\n----")
 
-            info_text2 = re.sub(r"^\[","",str(info_text))
-#            info_text2 = re.sub(r"^\[","",str(info_text).replace("\', \'","\',\n\'").replace('\'',"\""))
-            info_text3 = re.sub(r"\]$","",info_text2)
+                info_text2 = re.sub(r"^\[","",str(info_text))
+    #            info_text2 = re.sub(r"^\[","",str(info_text).replace("\', \'","\',\n\'").replace('\'',"\""))
+                info_text3 = re.sub(r"\]$","",info_text2)
 
-            failure_body = str(file_name)+"\n"+str(info_text3)
-            test_case = TestCase('Test1', None, 0)
-            test_case.add_failure_info(message=cve_message, output=failure_body)
-            test_cases_list.append(test_case)
+                failure_body = str(file_name)+"\n"+str(info_text3)
+                test_case = TestCase('Test1', None, 0)
+                test_case.add_failure_info(message=cve_message, output=failure_body)
+                test_cases_list.append(test_case)
 
 ts = TestSuite("my test suite", test_cases_list)
 # pretty printing is on by default but can be disabled using prettyprint=False
